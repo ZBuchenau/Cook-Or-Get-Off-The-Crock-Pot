39ceb1c54c97ad609962868e1ccf633c20e41db8
Brandon H Bodine <brandonhbodine@gmail.com>
2016-02-23 16:34:54 -0700

    changed format of json data

--- seeds/ingredients.js
+++ seeds/ingredients.js
@@ -1,16 +1,9 @@
-
+'use strict';
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
     knex('table_name').del(),
+    knex('ingredients').insert({ name: 'string', aisle : 'string'})
 
-    // Inserts seed entries
-    knex('table_name').insert({
-      id: 1,
-      colName: 'rowValue'
-    }),
-    
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
   );
 };
--- seeds/recipe_ingredients.js
+++ seeds/recipe_ingredients.js
@@ -1,8 +1,8 @@
-
+'use strict';
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('table_name').del(), 
+    knex('table_name').del(),
 
     // Inserts seed entries
     knex('table_name').insert({id: 1, colName: 'rowValue'}),
