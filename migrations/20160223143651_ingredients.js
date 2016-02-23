ee02a936a1cc111a4daec71c3cd6ef26afdf7ade
Zach Buchenau <zbuchenau@yahoo.com>
2016-02-23 15:01:52 -0700

    added bare bones for creating database schema

--- migrations/20160223143651_ingredients.js
+++ migrations/20160223143651_ingredients.js
@@ -1,8 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('ingredients', function(table){
+
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('ingredients');
 };
--- migrations/20160223143758_recipe_ingredients.js
+++ migrations/20160223143758_recipe_ingredients.js
@@ -1,8 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipe_ingredients', function(table){
+
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('recipe_ingredients');
 };
