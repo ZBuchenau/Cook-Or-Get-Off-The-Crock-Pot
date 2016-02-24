89983637defd3a1ed69c6773ec3a9c47c0bfea67
James Shieldsclear <James80401@gmail.com>
2016-02-24 10:19:02 -0700

    Added random knex query for the recipe retrieval

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,15 +1,18 @@
+'use strict'
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.json('API recipes');
 });
 
-router.get('/random', function(req, res, next) {
-  res.json('API recipes/:recipeId');
+router.get('/random', function(req, res) {
+  knex.raw("SELECT * FROM recipes ORDER BY RANDOM() LIMIT 5").then(function(randomRecipe){
+    res.json(randomRecipe.rows);
+  });
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function(req, res) {
   res.json('API recipes/:recipeId');
 });
 
