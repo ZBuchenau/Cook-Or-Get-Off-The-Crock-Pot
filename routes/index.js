aaf948b8a97bfd2ac43070306f657650a594ff6b
KeenanHoffman <keenan.m.hoffman@gmail.com>
2016-02-29 10:14:15 -0700

    added response to /

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  // res.json('API Home');
-
-  
-
+  res.json('API Home');
 });
 
 module.exports = router;
